f50b627dbc580d66fc0db5c1549b9b9248462e18
Vasudev Singhal <vasudevcoder@gmail.com>
2020-10-20 20:35:38 +0530

    Question 1 Complete

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,50 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Vasudev Singhal
+   Roll_Number: 2019126 */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int a =10;
+
+int main(){
+   pid_t pid;
+  
+
+   pid = fork();
+   if (pid<0)
+   {
+      fprintf(stderr, "Fork Failed");
+      return 1;
+   }
+   else if (pid == 0)
+   {
+      printf("Child Process \n");
+      while (a>-90)
+      {
+         printf("%d \n",a);
+         a--;
+         
+      }
+      printf("%d \n", a);
+      exit(0);
+      
+   }
+   else
+   {
+      wait(NULL);
+      printf("Parent Process \n");
+      while (a<100)
+      {
+         printf("%d \n",a);
+         a++;
+         
+      }
+      printf("%d \n", a);
+   }
+   
+   return 0;
+   
+}
--- Q1/Q1_part2.c
+++ Q1/Q1_part2.c
@@ -1,2 +1,32 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Vasudev Singhal
+   Roll_Number: 2019126 */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int a =10;
+
+void* child(void* arg){
+   printf("Child Process \n");
+   while (a>-90)
+   {
+      printf("%d \n",a);
+      a--;  
+   }
+   printf("%d \n", a);
+   pthread_exit(NULL);
+}
+
+int main(){
+   pthread_t ptid;
+   pthread_create(&ptid, NULL, &child, NULL);
+   pthread_join(ptid, NULL);
+   printf("Parent Process \n");
+   while (a<100)
+   {
+      printf("%d \n",a);
+      a++;
+      
+   }
+   printf("%d \n", a);
+}
