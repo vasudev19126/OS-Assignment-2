ea527b417b72f769bb4fdb010f3d1e074e5bb1f1
vasudev19126 <vasudev19126@iiitd.ac.in>
2020-10-27 08:06:48 -0700

    Kernal has been updated and sys call was added successfully

--- Q2/test.c
+++ Q2/test.c
@@ -1,2 +1,17 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Vasudev Singhal
+   Roll_Number: 2019126 */
+#define GNU SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+#define SYS_sh_task_info 440
+
+int main(int argc, char **argv){
+   // long pid = argv[1];
+   // char *file = argv[2];
+   // printf("Making system call with %ld \n", pid);
+   long res = syscall(SYS_sh_task_info);
+   printf("System call returned %ld \n", res);
+   return res;
+}
